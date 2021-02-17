2bbe7567f4163322d063d93bfd502de6a39270ea
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2021-02-17 07:51:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const imgs= document.getElementById("imgs");
+
+const img = document.querySelectorAll("#imgs img");
+
+let idx=0;
+function run(){
+    idx+=1;
+    if (idx > img.length-1){
+        idx=0;
+    }
+    imgs.style.transform= `translateY(${-idx *500}px)`;
+    setTimeout(run,2000)
+}
+run();
